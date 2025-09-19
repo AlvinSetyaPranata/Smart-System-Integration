1235ffbd4f2170523959ffc7602dcb84842b923e
AlvinSetyaPranata <comand.alvin@gmail.com>
2025-09-19 18:34:26 +0700

    UPDATE: Initialize flutter project

--- backend/app/routers/data.py
+++ backend/app/routers/data.py
@@ -1,7 +1,10 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 
 router = APIRouter()
 
 @router.post("/upload", tags=["uploads"])
-async def upload_data():
+async def upload_data(request: Request):
+
+    print(await request.body())
+
     return [{"username" : "Alvin1"}, {"username" : "Yudi"}]
